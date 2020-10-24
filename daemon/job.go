061bbe9b6fd33beb6de8022b2b85935749d278db
ericyan <eric7578@gmail.com>
2020-10-25 01:22:56 +0800

    Add callback to createJob

--- daemon/job.go
+++ daemon/job.go
@@ -1,6 +1,10 @@
 package daemon
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,15 +12,53 @@ import (
 
 func (d *Daemon) createJob(c *gin.Context) {
 	body := struct {
-		Type string
-		Args map[string]string
+		Type     string
+		Args     map[string]string
+		Callback string
 	}{}
 	c.BindJSON(&body)
 
-	res, err := d.crawler.CreateJob(body.Type, body.Args)
+	if body.Callback == "" {
+		res, err := d.crawler.CreateJob(body.Type, body.Args)
+		if err != nil {
+			panic(err)
+		}
+		c.JSON(http.StatusOK, res)
+		return
+	}
+
+	go func() {
+		res, _ := d.crawler.CreateJob(body.Type, body.Args)
+		d.doCallback(body.Callback, res)
+	}()
+	c.Status(http.StatusOK)
+}
+
+func (d *Daemon) doCallback(url string, body interface{}) error {
+	var bodyReader io.Reader
+	if body != nil {
+		payload, err := json.Marshal(body)
+		if err != nil {
+			panic(err)
+		}
+		bodyReader = bytes.NewReader(payload)
+	}
+
+	c := http.Client{}
+	req, err := http.NewRequest("POST", url, bodyReader)
 	if err != nil {
 		panic(err)
 	}
 
-	c.JSON(http.StatusOK, res)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return fmt.Errorf("request to %s got status code error: [%d] %s", url, res.StatusCode, res.Status)
+	}
+	return nil
 }
